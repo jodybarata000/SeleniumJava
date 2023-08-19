7fd155746852884e2f55c9984ffc2b5c5bd50c8f
Jody Barata Dwi Pradipta <jodybarata@gmail.com>
2023-08-19 17:22:17 +0700

    Fix issue not compatible browser

--- src/test/java/tests/utils/driver/DriverBuilder.java
+++ src/test/java/tests/utils/driver/DriverBuilder.java
@@ -1,5 +1,6 @@
 package tests.utils.driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,6 +9,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class DriverBuilder {
     public static String isHeadless = System.getProperty("headless", "false");
     public static WebDriver setupDriver() {
+        WebDriverManager.chromedriver().setup();
         DesiredCapabilities dc = new DesiredCapabilities();
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--window-size=1366,768");
@@ -15,6 +17,7 @@ public class DriverBuilder {
         chromeOptions.addArguments("--disable-notifications");
         chromeOptions.addArguments("--no-sandbox");
         chromeOptions.addArguments("--disable-dev-shm-usage");
+        chromeOptions.addArguments("--remote-allow-origins=*");
 
         if (isHeadless.equals("true")) {
             chromeOptions.addArguments("--headless");
